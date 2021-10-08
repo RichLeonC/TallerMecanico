0d508b76d1581f796846b9efc4ffdca4c7698202
RichLeonC <r29leonc@gmail.com>
2021-10-08 11:19:20 -0600

    Creado AdmClientes

--- Programa/TallerMecanicoWeb/src/java/Control/AdmClientes.java
+++ Programa/TallerMecanicoWeb/src/java/Control/AdmClientes.java
@@ -5,10 +5,49 @@
  */
 package Control;
 
+import Config.Conexion;
+import Modelo.Cliente;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 /**
  *
  * @author richa
  */
 public class AdmClientes {
+    private PreparedStatement ps;
+    private ResultSet rs;
+    private Connection con;
+    private Conexion conex = new Conexion();
+
+    public AdmClientes() {
+      
+    }
     
+    
+    
+    public ArrayList listar(){
+        ArrayList<Cliente> listaClientes = new ArrayList();
+        String sql = "select * from Cliente";
+        try {
+            con = conex.getConnection();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                Cliente cliente = new Cliente();
+                cliente.setCedula(rs.getInt("cedula"));
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setApellido2(rs.getString("apellido2"));
+                cliente.setDireccion(rs.getString("direccion"));
+                cliente.setTelefono(rs.getInt("telefono"));
+                listaClientes.add(cliente);
+            }
+           
+        } catch (Exception e) {
+        }
+         return listaClientes;
+    }
 }
